2e1c024949f8516c760f4c0ce55d3ffbd7e52347
kevin <zakevin96@hotmail.it>
2023-04-05 20:10:50 +0200

    asd

--- Module00/ex01/Program.java
+++ Module00/ex01/Program.java
@@ -6,7 +6,7 @@
 /*   By: kzak <kzak@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 11:38:39 by kzak              #+#    #+#             */
-/*   Updated: 2023/04/05 18:31:35 by kzak             ###   ########.fr       */
+/*   Updated: 2023/04/05 20:09:55 by kzak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,50 +14,29 @@ import java.util.Scanner;
 
 public class Program {
 
-	public static int addPutNumber(int number, int plus) {
-		boolean isNegative = false;
-
-		if (number < 0) {
-			isNegative = true;
-			number = -number;
-		}
-		if (number >= 10) {
-			plus = addPutNumber(number / 10, plus);
-		}
-		plus += number % 10;
-		if (isNegative) {
-			plus = -plus;
-		}
-		return plus;
-	}
-
 	public static void main(String[] argv) {
-		Scanner	scanner = new Scanner(System.in);
-		int		currentValue = scanner.nextInt();
-		int		coffeCounter = 0;
+		Scanner scanner = new Scanner(System.in);
+		int currentValue = scanner.nextInt();
 
-		while (currentValue != 42) {
-			if (currentValue < 1) {
-				scanner.close();
-				System.err.println("IllegalArgument");
-				System.exit(-1);
-			}
+		if (currentValue < 2) {
+			scanner.close();
+			System.err.println("IllegalArgument");
+			System.exit(-1);
+		}
 
-			int digitSum = addPutNumber(currentValue, 0);
-			boolean isPrime = true;
-	
-			for (int i = 2; i <= Math.sqrt(digitSum); i++) {
-				if (digitSum % i == 0) {
-					isPrime = false;
-				}
+		boolean isPrime = true;
+		int steps = 1;
+
+		for (int i = 2; i <= Math.sqrt(currentValue); i++) {
+			if (currentValue % i == 0) {
+				isPrime = false;
+				break;
 			}
-			if (isPrime == true)
-				coffeCounter++;
-			currentValue = scanner.nextInt();
+			steps++;
 		}
 
 		scanner.close();
-		System.out.println("Count of coffee - request -" + coffeCounter);
+		System.out.println(isPrime + " " + steps);
 		System.exit(0);
 	}
 }
