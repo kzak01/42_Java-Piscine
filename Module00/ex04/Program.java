6fa97e444f243925bd50fafcdca690ab03047535
kevin <zakevin96@hotmail.it>
2023-04-12 23:46:02 +0200

    ex04

--- Module00/ex04/Program.java
+++ Module00/ex04/Program.java
@@ -6,7 +6,7 @@
 /*   By: kzak <kzak@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 21:15:28 by kzak              #+#    #+#             */
-/*   Updated: 2023/04/12 23:37:55 by kzak             ###   ########.fr       */
+/*   Updated: 2023/04/12 23:45:08 by kzak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,51 +23,50 @@ public class Program {
 			unicodeBMP[c]++;
 		
 		char[]	mostFrequentChar = new char[10];
-		int[]	unicodePositions = new int[10];
+		int[]	frequencies = new int[10];
 
-		getMaxOccurrences(unicodeBMP, mostFrequentChar, unicodePositions);
+		getMaxOccurrences(unicodeBMP, mostFrequentChar, frequencies);
 
-		if (unicodePositions[0] > 999) {
+		if (frequencies[0] > 999) {
 			scanner.close();
 			System.err.println("Illegal Argument");
 			System.exit(-1);
 		}
 
-		printHistogram(mostFrequentChar, unicodePositions);
+		printHistogram(mostFrequentChar, frequencies);
 
 		scanner.close();
 		System.exit(0);
 	}
 
-	public static void	getMaxOccurrences(int[] unicodeBMP, char[] mostFrequentChar, int[] unicodePositions) {
-		char	maxChar = ' ';
-		int		maxCount = 0;
+	public static void	getMaxOccurrences(int[] unicodeBMP, char[] mostFrequentChar, int[] frequencies) {
+		char	mostFrequentCharacter = ' ';
+		int		maxFrequency = 0;
 		int		position = 0;
 		for (int i = 0; i < mostFrequentChar.length; i++) {
 			for (int j = 0; j < unicodeBMP.length; j++) {
-				if (unicodeBMP[j] > maxCount) {
-					maxCount = unicodeBMP[j];
-					maxChar = (char) j;
+				if (unicodeBMP[j] > maxFrequency) {
+					maxFrequency = unicodeBMP[j];
+					mostFrequentCharacter = (char) j;
 					position = j;
 				}
 			}
-			unicodePositions[i] = unicodeBMP[position];
-			mostFrequentChar[i] = maxChar;
+			frequencies[i] = unicodeBMP[position];
+			mostFrequentChar[i] = mostFrequentCharacter;
 			unicodeBMP[position] = 0;
-			maxChar = ' ';
-			maxCount = 0;
+			maxFrequency = 0;
 		}
 	}
 
-	public static void	printHistogram(char[] mostFrequentChar, int[] unicodePositions) {
-		int	maxRepetition = unicodePositions[0];
+	public static void	printHistogram(char[] mostFrequentChar, int[] frequencies) {
+		int	maxFrequencyCount = frequencies[0];
 
 		System.out.println();
 		System.out.println();
 
 		for (int i = 0; i < 10; i++) {
-			if (unicodePositions[i] == maxRepetition) {
-				System.out.print(unicodePositions[i]);
+			if (frequencies[i] == maxFrequencyCount) {
+				System.out.print(frequencies[i]);
 				System.out.print("   ");
 			}
 		}
@@ -75,12 +74,12 @@ public class Program {
 
 		for (int i = 10; i > 0; i--) {
 			for (int j = 0; j < 10; j++) {
-				if (unicodePositions[j] * 10 / maxRepetition >= i)
+				if (frequencies[j] * 10 / maxFrequencyCount >= i)
 					System.out.print("#   ");
-				if (unicodePositions[j] * 10 / maxRepetition == i - 1) {
-					if (unicodePositions[j] != 0) {
-						System.out.print(unicodePositions[j]);
-						if (unicodePositions[j] > 10)
+				if (frequencies[j] * 10 / maxFrequencyCount == i - 1) {
+					if (frequencies[j] != 0) {
+						System.out.print(frequencies[j]);
+						if (frequencies[j] > 10)
 							System.out.print("  ");
 						else
 							System.out.print("   ");
